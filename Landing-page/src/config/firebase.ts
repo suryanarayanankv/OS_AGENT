e8c174d6418c496a19450890ae88fe15e12e544f
tecrade <jovinjohn.developer@gmail.com>
2025-06-20 15:20:18 +0530

    connected to fire store

--- Landing-page/src/config/firebase.ts
+++ Landing-page/src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1-m6sbveASRS9U_VUb4LNyGWCocGcUqc",
@@ -20,5 +21,8 @@ export const auth = getAuth(app);
 
 // Initialize Analytics
 export const analytics = getAnalytics(app);
+
+// Initialize Firestore and export
+export const db = getFirestore(app);
 
 export default app;
